2f2e3db0128aed020568d38b2da5e40660d95961
Vineth Siriwardana <vineth.siriwardana@kcl.ac.uk>
2023-12-07 13:15:17 +0000

    Add feature that raises ValueError when the parameter passed is 0 or less.

--- primes.py
+++ primes.py
@@ -2,5 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    if number_of_primes > 0:
+        list = []
+        counter = 2
+        while len(list) < number_of_primes:
+            divisible_numbers = 0
+            for i in range(counter):
+                if counter % (i+1) == 0:
+                    divisible_numbers += 1
+            if divisible_numbers == 2:
+                list.append(counter)
+            counter += 1
+        print(list)
+    else:
+        raise ValueError("Input must be a positive integer!")
     return list
